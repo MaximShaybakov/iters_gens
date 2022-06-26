7a09e731d50b4565edf714bea596e26c3e8bdbec
MaximShaybakov <momon_boton@mail.ru>
2022-06-27 00:59:00 +0500

    Initial commit

---
+++ generator.py
@@ -0,0 +1,24 @@
+from itertools import chain
+
+
+nested_list = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f', 'h', False],
+    [1, 2, None],
+]
+
+def one_list(ls: list) -> list:
+    chain(*nested_list)
+    res = [elem for elem in chain(*nested_list)]
+    return res
+
+
+def show():
+    for obj in one_list():
+        yield obj
+        obj += 1
+
+
+if __name__ == '__main__':
+    for item in one_list(nested_list):
+        print(item)
